d3eae4e0ac7300641c28118806ef1c4c8c3ebba3
mostak9 <mostak.ahm263@gmail.com>
2023-10-17 02:20:24 +0600

    add get method to pass all user

--- index.js
+++ index.js
@@ -36,6 +36,12 @@ async function run() {
         res.send(result);
     })
 
+    app.get('/users', async(req, res)  => {
+        const cursor = usersCollection.find();
+        const result = await cursor.toArray();
+        res.send(result);
+    })
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
